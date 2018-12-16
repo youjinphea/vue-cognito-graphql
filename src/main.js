4ed0ea0c4146159a639cd8b373d981048d3ae301
yutoogi <yuto.ogi@fourier.jp>
2018-12-16 15:27:54 +0900

    add Authentication Form by AWS Cognito

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Amplify, { Auth } from 'aws-amplify'
+import aws_exports from './aws-exports'
+import router from './router'
+import store from './store'
+Amplify.configure(aws_exports)
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Auth.vue
@@ -0,0 +1,27 @@
+<template>
+  <amplify-authenticator v-bind:authConfig="authConfig"></amplify-authenticator>
+</template>
+
+<script>
+import { components } from 'aws-amplify-vue'
+
+export default {
+  data: () => {
+    return {
+      authConfig: {
+        signUpConfig: {
+          signUpFields: [
+            {
+              label: 'Email',
+              key: 'email',
+              required: true,
+              type: 'string'
+            }
+          ]
+        }
+      }
+    }
+  }
+}
+
+</script>
