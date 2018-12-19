25ffb4780b1bf6634ed7017a870a9cc151e521c5
yutoogi <yuto.ogi@fourier.jp>
2018-12-20 00:15:48 +0900

    S3へ画像アップロードできるように

--- src/views/Database.vue
+++ src/views/Database.vue
@@ -4,10 +4,9 @@
       <b-col md="6" class="py-3">
         <div class="text-center my-3">
           <b-form @submit="createPost" @reset="onReset">
-            <b-form-group label="Name:">
+            <!-- <b-form-group label="Name:">
               <b-form-input type="text" v-model="postData.name" required placeholder="Name"></b-form-input>
-            </b-form-group>
-
+            </b-form-group>-->
             <b-form-textarea
               v-model="postData.description"
               placeholder="Enter something"
@@ -15,20 +14,46 @@
               :max-rows="6"
             ></b-form-textarea>
 
+            <!-- <amplify-photo-picker v-bind:photoPickerConfig="photoPickerConfig"></amplify-photo-picker> -->
+            <div class="form-group">
+              <label for="imageFile">Image</label>
+              <input type="file" class="form-control-file" id="imageFile">
+            </div>
+
             <b-button type="submit" variant="primary">Submit</b-button>
             <b-button type="reset" variant="danger">Reset</b-button>
           </b-form>
         </div>
       </b-col>
       <b-col md="6" class="py-3">
-        <b-table :items="todos"></b-table>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="todo in todos" v-bind:key="todo.id">
+              <td>
+                <amplify-s3-image :imagePath="todo.name"></amplify-s3-image>
+                {{ todo.name }}
+              </td>
+              <td>{{ todo.description }}</td>
+            </tr>
+          </tbody>
+        </table>
       </b-col>
+    </b-row>
+
+    <b-row class="text-center">
+      <amplify-s3-album path="uploads"></amplify-s3-album>
     </b-row>
   </b-container>
 </template>
 
 <script>
-import { API, graphqlOperation } from "aws-amplify";
+import { API, graphqlOperation, Storage } from "aws-amplify";
 import * as subscriptions from "../graphql/subscriptions.js";
 import * as mutations from "../graphql/mutations.js";
 import * as queries from "../graphql/queries.js";
@@ -44,7 +69,10 @@ export default {
       },
       myPost: "",
       errors: [],
-      todos: []
+      todos: [],
+      photoPickerConfig: {
+        path: "upload"
+      }
     };
   },
   mounted: function() {
@@ -81,8 +109,22 @@ export default {
     });
   },
   methods: {
-    createPost: function(event) {
+    createPost: async function(event) {
       event.preventDefault();
+
+      console.log("event", event);
+
+      // Storage.put(event.target[3].files[0], event.target[3].files[0].name)
+      //   .then(result => console.log(result))
+      //   .catch(err => console.log(err));
+
+      const result = await Storage.put(
+        event.target[1].files[0].name,
+        event.target[1].files[0]
+      );
+      console.log(result);
+
+      this.$data.postData.name = result.key;
 
       API.graphql(
         graphqlOperation(mutations.createTodo, { input: this.$data.postData })
